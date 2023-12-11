cfed84f50298ae800cd42086e5b681734d7c4ff8
jhesso <jhesso.dev@gmail.com>
2023-12-11 15:28:47 +0200

    ex00

---
+++ m0/ex00/megaphone.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhesso <jhesso@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/11 15:14:15 by jhesso            #+#    #+#             */
+/*   Updated: 2023/12/11 15:23:06 by jhesso           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int	main(int ac, char **av)
+{
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		for (int i = 1; i < ac; i++)
+		{
+			for (int j = 0; av[i][j]; j++)
+				std::cout << (char)std::toupper(av[i][j]);
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
