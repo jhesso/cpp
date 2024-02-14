b0c1ea193b38a9e8038792cb2fb2a480c822ba26
jhesso <jhesso.dev@gmail.com>
2024-02-14 22:30:30 +0200

    v1 of array done

--- cpp07/ex02/include/Array.hpp
+++ cpp07/ex02/include/Array.hpp
@@ -6,13 +6,73 @@
 /*   By: jhesso <jhesso@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 19:34:04 by jhesso            #+#    #+#             */
-/*   Updated: 2024/02/13 19:34:18 by jhesso           ###   ########.fr       */
+/*   Updated: 2024/02/14 19:31:55 by jhesso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
 
+# include <iostream>
 
+template <typename T = int>
+class Array
+{
+	private:
+		T *				_array;
+		unsigned int	_size;
+
+	public:
+		Array(void) : _array(NULL), _size(0) {}
+		Array(unsigned int n) : _array(new T[n]), _size(n) {}
+		Array(Array const & src) : _array(new T[src._size]), _size(src._size)
+		{
+			for (unsigned int i = 0; i < this->_size; i++)
+				this->_array[i] = src._array[i];
+		}
+		~Array(void) { delete [] _array; }
+
+		Array &	operator=(Array const & src)
+		{
+			if (this == &src)
+				return *this;
+			if (this->_size != src._size)
+			{
+				delete [] _array;
+				this->_size = src._size;
+				this->_array = new T[this->_size];
+			}
+			for (unsigned int i = 0; i < this->_size; i++)
+				this->_array[i] = src._array[i];
+			return *this;
+		}
+
+		class OutOfBoundsException : public std::exception {
+			public:
+				virtual char const *	what(void) const throw() {
+					return ("Index out of bounds.");
+				}
+		};
+
+		T &	operator[](unsigned int index)
+		{
+			if (index >= this->_size)
+				throw (OutOfBoundsException());
+			return this->_array[index];
+		}
+
+		unsigned int	size(void) const
+		{
+			return this->_size;
+		}
+};
+
+template <typename T>
+std::ostream &	operator<<(std::ostream & os, Array<T> & obj)
+{
+	for (unsigned int i = 0; i < obj.size(); i++)
+		os << "[" << obj[i] << "]\n";
+	return os;
+}
 
 #endif
--- cpp07/ex02/src/main.cpp
+++ cpp07/ex02/src/main.cpp
@@ -6,14 +6,123 @@
 /*   By: jhesso <jhesso@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 19:33:42 by jhesso            #+#    #+#             */
-/*   Updated: 2024/02/13 19:34:46 by jhesso           ###   ########.fr       */
+/*   Updated: 2024/02/14 20:21:02 by jhesso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cstdlib>
 #include "Array.hpp"
+
+# define	GREEN	"\033[0;32m"
+# define	YELLOW	"\033[0;33m"
+# define	BLUE	"\033[0;34m"
+# define	RED		"\033[0;31m"
+# define	RESET	"\033[0m"
 
 int	main(void)
 {
+	Array<int> arr(500);
+	int			cpy[500];
+
+	std::cout << "filling array 'arr' with 500 random values" << std::endl;
+	for (int i = 0; i < 500; i++)
+	{
+		int n = rand();
+		arr[i] = n;
+		cpy[i] = n;
+	}
+	for (int i = 0; i < 500; i++)
+	{
+		if (cpy[i] != arr[i])
+		{
+			std::cout << "values are different!" << std::endl;
+			return 1;
+		}
+	}
+	std::cout << std::endl << YELLOW << "---- TEST ACCESSING GOOD/BAD INDEXES ----"
+				<< RESET << std::endl << std::endl;
+	try
+	{
+		std::cout << BLUE << "testing with index: 42" << RESET << std::endl;
+		std::cout << "arr[42]: [" << arr[42] << "]" << std::endl;
+		std::cout << "TEST: arr[42] = 42" << std::endl;
+		arr[42] = 42;
+		std::cout << "arr[42]: [" << arr[42] << "]" << std::endl;
+		std::cout << GREEN << "SUCCESS!" << RESET << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << std::endl << RED << "Exception: " << e.what() << RESET << std::endl;
+	}
+	try
+	{
+		std::cout << std::endl << BLUE << "testing with index: -42" << RESET << std::endl;
+		std::cout << "arr[-42]: [" << "OUT OF BOUNDS" << "]" << std::endl;
+		std::cout << "TEST: arr[-42] = -42" << std::endl;
+		arr[-42] = -42;
+		std::cout << "arr[-42]: [" << arr[-42] << "]" << std::endl;
+		std::cout << GREEN << "SUCCESS!" << RESET << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << RED << "Exception: " << e.what() << RESET << std::endl;
+	}
+	try
+	{
+		std::cout << std::endl << BLUE << "testing with index 0" << RESET << std::endl;
+		std::cout << "arr[0]: [" << arr[0] << "]" << std::endl;
+		std::cout << "TEST: arr[0] = 0" << std::endl;
+		arr[0] = 0;
+		std::cout << "arr[0]: [" << arr[0] << "]" << std::endl;
+		std::cout << GREEN << "SUCCESS!" << RESET << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << RED << "Exception: " << e.what() << RESET << std::endl;
+	}
+
+	std::cout << std::endl << YELLOW << "---- TEST CONSTRUCTORS ----" << RESET << std::endl << std::endl;
+	try
+	{
+		Array<int> a(10);
+		Array<int> b(5);
+		Array<int> c(a);
+		Array<int> d;
+
+		for (int i = 0; i < 10; i++)
+			a[i] = i;
+		for (int i = 0; i < 5; i++)
+			b[i] = i;
+		for (unsigned int i = 0; i < c.size(); i++)
+			c[i] = i;
+		d = b;
+
+		std::cout << BLUE << "TEST: default constructor (Array<int> a(10))" << RESET << std::endl;
+		std::cout << "a contents:" << std::endl << a;
+		std::cout << BLUE << "TEST: copy constructor (Array c(a))" << RESET << std::endl;
+		std::cout << "c contents:" << std::endl << c;
+		std::cout << BLUE << "TEST: copy assignment operator (d = b)" << RESET << std::endl;
+		std::cout << "b contents:" << std::endl << b;
+		std::cout << "d contents:" << std::endl << d;
+		std::cout << std::endl << BLUE << "TEST: change values in copies" << RESET << std::endl;
+		std::cout << "a[1]: " << a[1] << std::endl;
+		std::cout << "c[1]: " << c[1] << std::endl;
+		std::cout << "c[1] = rand()" << std::endl;
+		c[1] = rand();
+		std::cout << "a[1]: " << a[1] << std::endl;
+		std::cout << "c[1]: " << c[1] << std::endl << std::endl;
+		std::cout << "b[1]: " << b[1] << std::endl;
+		std::cout << "d[1]: " << d[1] << std::endl;
+		std::cout << "d[1] = rand()" << std::endl;
+		d[1] = rand();
+		std::cout << "b[1]: " << b[1] << std::endl;
+		std::cout << "d[1]: " << d[1] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << RED << "Exception: " << e.what() << RESET << std::endl;
+	}
+
 	return 0;
 }
